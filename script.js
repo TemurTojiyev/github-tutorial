c4363b9b88a285793dd371ec1c40969686b6073a
Temur <temurtojiyev11@gmail.com>
2023-09-05 23:38:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// "use strict";
+
+// Number
+// let number = 4.5; // data type number
+// console.log(8 / 0); // data type infinity
+// console.log("Temur" * 4); // data type NaN
+
+// String
+// const username = "Samar";
+
+// Boolean
+// const isMarried = true;
+// console.log(isMarried);
+
+let age = 10;
+
+let numberStudent = 20;
+
+console.log(++age);
